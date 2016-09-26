5456820b80fe883baff82f3cd1e32af88d5cb51c
AndyChuang <andychuang@cybersoft4u.com>
2016-09-27 01:23:10 +0800

    Create vm with winrm via https and register endpoint 5986

--- main.py
+++ main.py
@@ -1,29 +1,40 @@
 from azure import *
 from azure.servicemanagement import *
 
-subscription_id = '<YOUR_SUBSCRIPTION_ID_HERE>'
-certificate_path = 'CURRENT_USER\\my\\<YOUR_CERT_NAME>'
+subscription_id = 'TEST'
+certificate_path = 'CURRENT_USER\\my\\user-PC'
 
 sms = ServiceManagementService(subscription_id, certificate_path)
 
-service_name = 'TEST'
-image_name = 'TEST'
-vm_name = 'TEST'
+'''
+images = sms.list_vm_images()
+for image in images:
+    print image.name
+'''
+
+service_name = 'typhoon001wq'
+image_name = 'testServerSource'
+vm_name = 'typhoon001wq'
 location = "Japan West"
 
-windows_config = WindowsConfigurationSet(computer_name="TEST",
+windows_config = WindowsConfigurationSet(computer_name="SDD",
     admin_username = "TEST",
     admin_password = "TEST",
     reset_password_on_first_logon = False,
     enable_automatic_updates  = False)
 windows_config.domain_join = None
-windows_config.win_rm.listeners.listeners.append(Listener('Http'))
+windows_config.win_rm.listeners.listeners.append(Listener('Https'))
 
 network_config = ConfigurationSet()
 network_config.input_endpoints.input_endpoints.append(ConfigurationSetInputEndpoint(name='Remote Desktop',
                                                                                     protocol='TCP',
                                                                                     port='3389',
                                                                                     local_port='3389'))
+
+network_config.input_endpoints.input_endpoints.append(ConfigurationSetInputEndpoint(name='WinRM',
+                                                                                    protocol='TCP',
+                                                                                    port='5986',
+                                                                                    local_port='5986'))
 
 #Set the location
 sms.create_hosted_service(service_name=service_name,
