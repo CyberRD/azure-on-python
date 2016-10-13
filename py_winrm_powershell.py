6ddf316749db162a0410207e3f1e7541e5b8a6b3
andychuang <andychuang@cybersoft4u.com>
2016-10-13 11:11:24 +0800

    Update gitignore and remove .idea

--- py_winrm_powershell.py
+++ py_winrm_powershell.py
@@ -1,15 +1,17 @@
 import winrm
 
 ps_script = """
-pwd
+cd C:\chef-cybermars-test-auto-deploy\cookbooks
+chef-client --local-mode --runlist 'recipe[cybermars_test_auto_deploy_simple]'
 """
 
 '''
+for 開發環境
 cd C:\chef-cybermars-auto-deploy\cookbooks
 chef-client --local-mode --runlist 'recipe[cybermars_auto_deploy_simple]'
 '''
 
-s = winrm.Session('http://marstestclean1005.cloudapp.net:5985/wsman', auth=('SDD', 'XXX'))
+s = winrm.Session('http://marstestclean1012.cloudapp.net:5985/wsman', auth=('SDD', 'XXX'))
 r = s.run_ps(ps_script)
 
 print r.status_code
